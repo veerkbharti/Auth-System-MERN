5d6f526e126b8eb5905b273a4a18ff844ee3434e
Veer Kunwer Bharti <veer7549kr@gmail.com>
2023-12-21 01:39:18 +0530

    Added error handler and middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import errorMiddleware from "./middlewares/error.js";
 
 const app = express();
 dotenv.config({ path: ".env" });
-
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
 });
 
+app.use(errorMiddleware());
 
 export default app;
